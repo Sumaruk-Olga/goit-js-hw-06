aa40a70abf2bd3e075e30f60b4ae5324f9e85896
Olga Sumaruk <olga_sumaruk@hotmail.com>
2022-05-18 21:07:37 +0300

    done task 1
    done task 1

---
+++ js/task-01.js
@@ -0,0 +1,42 @@
+// Завдання 1
+
+// Напиши скрипт, який:
+
+// Порахує і виведе в консоль кількість
+//категорій в ul#categories, тобто елементів
+//li.item.
+// Для кожного элемента li.item у спику
+//ul#categories, знайде і виведе в консоль
+//текст заголовку елемента(тегу < h2 >) і
+// кількість елементів в категорії
+//(усіх < li >, вкладених в нього).
+// В результаті, в консолі будуть виведені
+//наступні повідомлення.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+
+const categories = document.querySelectorAll('.item');
+console.log('Number of categories: ', categories.length);
+
+const categoriesItem = document.querySelectorAll('.item');
+// console.log(categoriesItem);
+
+categoriesItem.forEach(
+    (item) => {
+        // console.log(item);
+        console.log('Category:', item.firstElementChild.textContent);
+        console.log('Elements:', item.lastElementChild.children.length);
+    });
+    
+
+
